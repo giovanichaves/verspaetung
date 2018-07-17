bad5475055f6c1fb35f9ac88e9cdae4646c55048
Giovani Chaves <giovanichaves@gmail.com>
2018-07-18 01:12:33 +0200

    Fixed deprecated adapter

--- src/main/java/com/mobimeo/verspaetung/VerspaetungApplication.java
+++ src/main/java/com/mobimeo/verspaetung/VerspaetungApplication.java
@@ -4,10 +4,10 @@ import com.mobimeo.verspaetung.api.converter.StringToLocalTimeConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class VerspaetungApplication extends WebMvcConfigurerAdapter {
+public class VerspaetungApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerspaetungApplication.class, args);
