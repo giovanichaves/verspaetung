6d3ff201385c7eb8156f5626cfc60e6a029a6789
Giovani Chaves <giovanichaves@gmail.com>
2018-07-23 10:32:50 +0200

    Stop id field

--- src/main/java/com/mobimeo/verspaetung/datasource/db/entities/Stop.java
+++ src/main/java/com/mobimeo/verspaetung/datasource/db/entities/Stop.java
@@ -1,6 +1,5 @@
 package com.mobimeo.verspaetung.datasource.db.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,15 +8,20 @@ import javax.persistence.*;
 
 @Getter
 @EqualsAndHashCode
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "stops")
 public class Stop {
+    public Stop(int stop_id, int x, int y) {
+        this.stop_id = stop_id;
+        this.x = x;
+        this.y = y;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    private int stop_id;
     private int x;
     private int y;
 }
