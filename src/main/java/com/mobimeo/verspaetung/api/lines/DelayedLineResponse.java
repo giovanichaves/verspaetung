3f1e3c4225c97340bf7b331df387f2530d3b6611
Giovani Chaves <giovanichaves@gmail.com>
2018-07-24 23:25:38 +0200

    ResponseBody on response comes from RestController

--- src/main/java/com/mobimeo/verspaetung/api/lines/DelayedLineResponse.java
+++ src/main/java/com/mobimeo/verspaetung/api/lines/DelayedLineResponse.java
@@ -1,9 +1,7 @@
 package com.mobimeo.verspaetung.api.lines;
 
 import lombok.Getter;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@ResponseBody
 @Getter
 public class DelayedLineResponse {
 
--- src/main/java/com/mobimeo/verspaetung/api/lines/VehiclesResponse.java
+++ src/main/java/com/mobimeo/verspaetung/api/lines/VehiclesResponse.java
@@ -1,11 +1,9 @@
 package com.mobimeo.verspaetung.api.lines;
 
 import lombok.Getter;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@ResponseBody
 @Getter
 public class VehiclesResponse {
 
